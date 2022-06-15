cdf17b8776cb6d544308bba55c5a76eb6634ac61
NQPhuc <11730168+NQPhuc@users.noreply.github.com>
2022-06-16 03:30:28 +0700

    Fix naming

--- packages/dbml-core/src/export/AmlExporter/table.js
+++ packages/dbml-core/src/export/AmlExporter/table.js
@@ -6,14 +6,14 @@ export function exportTable (tableId, model, dataSource) {
   const content = getFieldLines(tableId, model);
   const table = model.tables[tableId];
   const schema = model.schemas[table.schemaId];
-  const nameSchema = `${shouldPrintSchema(schema, model) ? `${schema.name}.` : 'public'}`;
-  const nameTable = `${nameSchema}_${table.name}`;
-  let tableStr = ` Model ${nameTable} {\n`;
+  const schemaName = `${shouldPrintSchema(schema, model) ? `${schema.name}.` : 'public'}`;
+  const modelName = `${schemaName}_${table.name}`;
+  let tableStr = ` Model ${modelName} {\n`;
   tableStr += '  type: \'table\'\n';
   tableStr += `  label: "${table.name}"\n`;
   tableStr += `  description: ${table.note ? `'${table.note}'` : ''}\n`;
   tableStr += `  data_source_name: '${dataSource}'\n`;
-  tableStr += `  table_name: '"${nameSchema}"."${table.name}"'\n`;
+  tableStr += `  table_name: '"${schemaName}"."${table.name}"'\n`;
   tableStr += `${content.map(line => `  ${line}`).join('\n')}\n}\n\n\n`;
-  return { name: nameTable, content: tableStr };
+  return { name: modelName, content: tableStr };
 }
