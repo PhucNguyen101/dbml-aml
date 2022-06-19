49f4d90b71bf3e988fb172dd6597901e2d73da83
PhucNguyen101 <phuc.nguyen@holistics.io>
2022-06-19 19:15:46 +0700

    define aml to dbml raw db functions

--- packages/dbml-core/src/parse/aml/codegen/dataset.js
+++ packages/dbml-core/src/parse/aml/codegen/dataset.js
@@ -2,9 +2,19 @@ import { convertModelToDbmlRawTable } from './model';
 import { convertRelationshipToDbmlRawRef } from './relationship';
 
 export function convertDatasetToDbmlRawDB (dataset) {
-  const res = {
+  const { tables, fields } = dataset.data_models.reduce((acc, ele) => {
+    const raw = convertModelToDbmlRawTable(ele);
+    acc.tables.push(raw.table);
+    acc.fields.push(...raw.fields);
+    return acc;
+  }, {
     tables: [],
-    refs: [],
+    fields: [],
+  });
+  const res = {
+    tables,
+    fields,
+    refs: dataset.raw_relationships.map(r => convertRelationshipToDbmlRawRef(r)),
     project: {
       name: dataset.name || 'undefined',
       note: dataset.description,
--- packages/dbml-core/src/parse/aml/codegen/model.js
+++ packages/dbml-core/src/parse/aml/codegen/model.js
@@ -1,5 +1,7 @@
 import { convertDimensionToDbmlRawField } from './dimension';
 
 export function convertModelToDbmlRawTable (model) {
-  return null;
+  const fields = model.dimensions.map(d => convertDimensionToDbmlRawField(d));
+  const table = { };
+  return { table, fields };
 }
